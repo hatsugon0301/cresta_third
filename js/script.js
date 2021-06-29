c49ebda6ff406a1f4c701bf635ef42d0de78aa53
hatsugon <>
2021-06-29 16:07:55 +0900

    使っていないファイル削除

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+  var fvHeight = $('.fv').outerHeight();
+  
+  $(window).on('load scroll',function(){
+
+  if($(this).scrollTop() > fvHeight ){
+    $('header').addClass('black-bg');
+  } else{
+    $('header').removeClass('black-bg');
+  }
+});
+  $('.menu-btn').on('click', function(){
+    $('.menu-btn').toggleClass('close');
+    $('.header-nav,.mask,.fv-content').toggleClass('open');
+  });
+}());
