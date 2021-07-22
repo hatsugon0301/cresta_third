36f8d1c7e9089c4fa7de0e2febafeb95d43de2be
hatsugon <>
2021-07-22 10:50:57 +0900

    no message

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-$(function(){
+$(function () {
   var fvHeight = $('.fv').outerHeight();
-  
-  $(window).on('load scroll',function(){
 
-  if($(this).scrollTop() > fvHeight ){
-    $('header').addClass('black-bg');
-  } else{
-    $('header').removeClass('black-bg');
-  }
-});
-  $('.menu-btn').on('click', function(){
+  $(window).on('load scroll', function () {
+
+    if ($(this).scrollTop() > fvHeight) {
+      $('header').addClass('black-bg');
+    } else {
+      $('header').removeClass('black-bg');
+    }
+  });
+  $('.menu-btn').on('click', function () {
     $('.menu-btn').toggleClass('close');
     $('.header-nav,.mask,.fv-content').toggleClass('open');
   });
